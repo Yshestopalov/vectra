678ccc8da67e9754da18f3c0cb7e33b3ce18b190
Yurii Shestopalov <yurii.shestopalov@gmail.com>
2026-09-11 23:46:31 -0700

    Exposed utils functions in the public API

--- src/vectra/__init__.py
+++ src/vectra/__init__.py
@@ -4,5 +4,22 @@ Public API for the vectra package.
 
 from .vector2d import Vector2D
 from .vector3d import Vector3D
+from .utils import (
+    lerp,
+    clamp,
+    remap,
+    radians_to_degrees,
+    degrees_to_radians,
+    approx_equal,
+)
 
-__all__ = ["Vector2D", "Vector3D"]
+__all__ = [
+    "Vector2D",
+    "Vector3D",
+    "lerp",
+    "clamp",
+    "remap",
+    "radians_to_degrees",
+    "degrees_to_radians",
+    "approx_equal",
+]
